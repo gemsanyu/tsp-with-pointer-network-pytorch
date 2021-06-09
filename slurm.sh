5caea98b7e6ef0d2f92eef5fb548589af89a82ec
gemilanghpc <git.gemilang.santiyuda@gmail.com>
2021-06-09 08:44:42 +0700

    run-in-hpc

---
+++ slurm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=tsp-pointer
+#SBATCH --output=logs/tsp_%A.out
+#SBATCH --error=logs/tsp_%A.err
+#
+#SBATCH --time=30-00:00:00
+#SBATCH --nodelist=komputasi08
+
+source ~/ttp/bin/activate;
+srun python train.py --title not_learnable &
+srun python train.py --learnable-first-input True --title learnable &
+srun tensorboard --logdir=runs &
+
+wait;
